316bc888aadf0f92f18f953e934fceb0212d1898
Maksim Volkau <maxred92@mail.ru>
2023-04-24 13:24:48 +0200

    Add edit and delete form

--- site_stock/product/forms.py
+++ site_stock/product/forms.py
@@ -24,3 +24,25 @@ class AddThingForm(forms.ModelForm):
             })
         }
 
+class EditThingForm(forms.ModelForm):
+    class Meta:
+        model = Things
+        fields = ('category', 'name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'price': forms.TextInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'image': forms.FileInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            })
+        }
+
--- site_stock/product/views.py
+++ site_stock/product/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Things
-from .forms import AddThingForm
+from .forms import AddThingForm, EditThingForm
 
 
 def detail(request, pk):
@@ -34,3 +34,30 @@ def add(request):
     }
 
     return render(request, 'product/new.html', context)
+
+
+@login_required
+def edit(request, pk):
+    things = get_object_or_404(Things, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditThingForm(request.POST, request.FILES, instance=things)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('product:detail', pk=things.id)
+    else:
+        form = EditThingForm(instance=things)
+
+    return render(request, 'product/new.html', {
+        'form': form,
+        'title': 'Edit item',
+    })
+
+@login_required
+def delete(request, pk):
+    things = get_object_or_404(Things, pk=pk, created_by=request.user)
+    things.delete()
+
+    return redirect('store:index')
