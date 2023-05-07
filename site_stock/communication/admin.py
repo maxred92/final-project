4dd5e9357c3c503ce2419e5c2deb0f9f03928d3f
Maksim Volkau <maxred92@mail.ru>
2023-05-07 16:59:59 +0200

    Updated admin panel

--- site_stock/communication/admin.py
+++ site_stock/communication/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Communication, Message
 
-admin.site.register(Communication)
-admin.site.register(Message)
+@admin.register(Communication)
+class CommunicationAdmin(admin.ModelAdmin):
+    list_display = ('things', 'created_at')
+    list_filter = ('created_at','modified_at')
+    search_fields = ('things', 'members')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('content', 'communication', 'created_by')
+    list_filter = ('created_by', 'communication', 'created_at')
+    search_fields = ('created_by', 'created_at')
--- site_stock/product/admin.py
+++ site_stock/product/admin.py
@@ -1,7 +1,50 @@
+from django.urls import reverse
+from django.utils.http import urlencode
 from django.contrib import admin
+from django.utils.html import format_html
+from django.db.models import Avg
+
 
 from .models import Category, Things
 
 
-admin.site.register(Category)
-admin.site.register(Things)
+@admin.register(Things)
+class ThingsAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    sortable_by = ('name', 'created_at', 'full_price', 'created_by')
+    list_filter = ('category',)
+    search_fields = ('category_name', 'name')
+    list_display = (
+        'name',
+        'created_at',
+        'full_price'
+    )
+
+
+    @admin.display(description='price')
+    def full_price(self,obj):
+        return f'{obj.price} $'
+
+class ThingsInCategory(admin.StackedInline):
+    model = Things
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'show_average')
+    inlines = (ThingsInCategory, )
+
+
+    @admin.display(description='things')
+    def view_things(self,obj):
+        count = obj.things_set.count()
+        url = (
+            reverse('admin:product_things_changelist')
+            + '?'
+            + urlencode({'category_id': f'{obj.id}'})
+        )
+        return format_html('<a href="{}">{} Things</a>', url, count)
+
+    @admin.display(description='avg_price')
+    def show_average(self, obj):
+        result = Things.objects.filter(category=obj).aggregate(Avg('price'))
+        return result['price__avg']
